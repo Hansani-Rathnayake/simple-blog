dd61665bf01014109d196dfc7cda090eb49e369c
Hansani123 <136361608+Hansani-Rathnayake@users.noreply.github.com>
2024-12-06 08:53:32 +0530

    update code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav>
-      <ul>
+      <ul className="blog-post">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/add">Add Post</Link></li>
         <li><Link to="/edit/1">Edit Post</Link></li> 
