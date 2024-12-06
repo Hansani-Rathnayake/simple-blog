4523a2f3767fe1d4e61200a5955380107b16c9ed
Hansani123 <136361608+Hansani-Rathnayake@users.noreply.github.com>
2024-12-06 16:31:33 +0530

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
       <ul className="nav-item-section">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/add">Add Post</Link></li>
-        <li><Link to="/edit/1">Edit Post</Link></li> 
+        {/* <li><Link to="/edit/1">Edit Post</Link></li>  */}
       </ul>
     </nav>
   );
